2553740cb8ac3ade576d218bfce09767d5c9f880
Georg Schmidl <georg.schmidl@vicox.net>
2015-01-06 21:38:48 +0100

    add header with usage and example

--- backup-to-afp.sh
+++ backup-to-afp.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Backups a directory to an AFP share with rsync.
+#
+# Usage: backup-to-afp.sh DIRECTORY AFP_URL
+#
+# Example: backup-to-afp.sh /path/to/MyDirectory afp://192.168.0.1/MyDirectoryBackup
+#
+# If you don't provide a username and password in the url it will check the OS X Keychain
+# for a password for the current user. If not found it will prompt for credentials.
+
 dir=$1
 afp_url=$2
 
