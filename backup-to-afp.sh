7e8a3fb8dc08fa2e3fbad2993e604214f6c70add
Georg Schmidl <georg.schmidl@vicox.net>
2015-01-06 20:24:05 +0100

    check osx keychain for password

--- backup-to-afp.sh
+++ backup-to-afp.sh
@@ -3,12 +3,27 @@ afp_url=$2
 
 afp_url_arr=(${afp_url//\// })
 
-sharepoint=${afp_url_arr[2]}
-mountpoint=/Volumes/$sharepoint
+server=${afp_url_arr[1]}
+volume=${afp_url_arr[2]}
 
+mountpoint=/Volumes/$volume
 
 mkdir $mountpoint
-mount_afp -i $afp_url $mountpoint
+
+if [[ $server == *"@"* ]]
+then
+  mount_afp -i $afp_url $mountpoint
+else
+  user=$USER
+  pwd=$(security 2>&1 >/dev/null find-internet-password -gs $server -a $user | ruby -e 'print $1 if STDIN.gets =~ /^password: "(.*)"$/')
+
+  if [ -n "$pwd" ]
+  then
+    mount_afp ${afp_url/\/\//\/\/$user:$pwd@} $mountpoint
+  else
+    mount_afp -i $afp_url $mountpoint
+  fi
+fi
 
 rsync -aP --delete --exclude=.DS_Store --exclude=.afpDeleted* $dir/ $mountpoint
 
